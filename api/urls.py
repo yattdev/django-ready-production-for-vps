7e73e230a0fcc77eaee8d2ac122079f03546c71a
yattdev <yattdeveloper@gmail.com>
2021-11-13 18:20:33 +0100

    Added blog endpoints (Article, Catagory) viewset to api/urls

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,10 @@
 
 from django.urls import path, include
 from .views.utils_view import get_csrf_token
+from .views.article_views import CategoryArticleList, ArticleViewSet
+from .views.category_views import CategoryViewSet
+
+from rest_framework.routers import SimpleRouter
 
 urlpatterns = [
     path('get-token/', get_csrf_token, name="get-token"),  # to get csrf_token 
@@ -12,3 +16,18 @@ urlpatterns = [
     path('auth', include('djoser.urls.jwt')),
     path('auth', include('djoser.urls.authtoken'), name="auth_token"),
 ]
+
+# ---------------
+# Blog endpoints
+
+router = SimpleRouter()
+
+# Article List/Details endpoint by viewsets
+router.register('articles', ArticleViewSet, basename="articles")
+
+# Category List/Details endpoint by viewsets
+router.register('categories', CategoryViewSet, basename='categories')
+
+blog_urls = [
+    path('<str:category_name>/', CategoryArticleList.as_view()),
+] + router.urls + urlpatterns
