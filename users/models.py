680ad25c641ae2134e8150faeae688e836e10c4c
yattdev <yattdeveloper@gmail.com>
2021-11-13 18:39:21 +0100

    Modif users/Userccount for username, email, password to registre
    - Drop database to and create a new

--- users/models.py
+++ users/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 
+USER_ROLES = [('Admin', 'Admin'), ('Staff', 'Staff'),
+              ('Utilisateur', 'Utilisateur')]
 
-USER_ROLES = [('Admin', 'Admin'), ('Staff', 'Staff')]
 
 class UserAccountManager(UserManager):
     """ DOCSTRING:
@@ -10,15 +11,25 @@ class UserAccountManager(UserManager):
     """
 
     # override create user method to accept email as username
-    def create_user(self, email=None, password=None, **extra_fields):
-        return super().create_user(email,
+    def create_user(self,
+                    username=None,
+                    email=None,
+                    password=None,
+                    **extra_fields):
+
+        return super().create_user(username=username,
                                    email=email,
                                    password=password,
                                    **extra_fields)
 
     # override createusuperuser method to accept email as username
-    def create_superuser(self, email=None, password=None, **extra_fields):
-        return super().create_superuser(email,
+    def create_superuser(self,
+                         username=None,
+                         email=None,
+                         password=None,
+                         **extra_fields):
+
+        return super().create_superuser(username=username,
                                         email=email,
                                         password=password,
                                         **extra_fields)
@@ -29,16 +40,18 @@ class UserAccount(AbstractUser):
         define a custom user models
     """
     email = models.EmailField(unique=True)
-    role = models.CharField(max_length=40, choices=USER_ROLES, default='Staff')
-    
-    USERNAME_FIELD = 'email'
+    role = models.CharField(max_length=40,
+                            choices=USER_ROLES,
+                            default='Utilisateur')
 
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    USERNAME_FIELD = 'username'
+
+    REQUIRED_FIELDS = ['email', 'password']
 
     objects = UserAccountManager()
 
     class Meta:
         ordering = ['-date_joined']
-        
+
     def __str__(self):
         return self.email.split('@')[0]
