cc3adfa0e1df2b66149208ed5a8bb4815a54f452
yattdev <yattdeveloper@gmail.com>
2021-11-15 19:47:53 +0100

    Rename test for user models to test_user_models and update test with factory_boy and faker

---
+++ users/tests/test_user_models.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from users.factories import UserFactory, AdminFactory
+from django.contrib.auth import get_user_model
+
+# Get Custom User
+User = get_user_model()
+
+
+class UserAccountTestCase(TestCase):
+    """ TestCase for UserAccount models"""
+    def setUp(self):
+        self.user = UserFactory()
+        self.admin = AdminFactory()
+
+    def test_if_users_created(self):
+        self.assertEqual(User.objects.count(), 2)
+
+    def test_user_content(self):
+        user = User.objects.get(id=self.user.id)
+
+        self.assertEqual(f'{user.first_name}', f'{self.user.first_name}')
+        self.assertEqual(f'{user.last_name}', f'{self.user.last_name}')
+        self.assertEqual(f'{user.password}', f'{self.user.password}')
+        self.assertEqual(f'{user.username}', f'{self.user.username}')
+        self.assertEqual(f'{user.email}', f'{self.user.email}')
+
+    def test_superuser_content(self):
+        superuser = User.objects.get(id=self.admin.id)
+
+        self.assertEqual(f'{superuser.first_name}', f'{self.admin.first_name}')
+        self.assertEqual(f'{superuser.last_name}', f'{self.admin.last_name}')
+        self.assertEqual(f'{superuser.password}', f'{self.admin.password}')
+        self.assertEqual(f'{superuser.username}', f'{self.admin.username}')
+        self.assertEqual(f'{superuser.email}', f'{self.admin.email}')
