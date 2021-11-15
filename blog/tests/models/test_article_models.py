97612e42d41f5ec895e339b9d704f83e731029a1
yattdev <yattdeveloper@gmail.com>
2021-11-15 23:04:02 +0100

    Override settimgs.MEDIA_ROOT to remove media file generate while testing and Added test_article_contents to test article content value

--- blog/tests/models/test_article_models.py
+++ blog/tests/models/test_article_models.py
@@ -1,25 +1,28 @@
+import os
+import shutil
 # Core Django imports.
+from django.conf import settings
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.urls import reverse
 from django.utils.text import slugify
 
 from blog.factories.article_factory import ArticleFactory
-
 # Blog application imports.
 from blog.models.article_models import Article
 
 
+@override_settings(MEDIA_ROOT=os.path.join(settings.BASE_DIR, 'media_dir_for_test/'))
 class ArticleTestCase(TestCase):
     """
       Class to test the article model.
     """
-
-    def setUp(self):
+    @classmethod
+    def setUpTestData(cls):
         """
           Set up all the tests using ArticleFactory
         """
-        self.article = ArticleFactory()
+        cls.article = ArticleFactory()
 
     def test_if_article_returns_the_right_human_readable_representation(self):
         self.assertEqual(self.article.__str__(), self.article.title)
@@ -31,3 +34,20 @@ class ArticleTestCase(TestCase):
         self.assertEquals(self.article.get_absolute_url(),
                           reverse('blog:article_detail', kwargs={'username': self.article.author.username.lower(),
                                                                  'slug': self.article.slug}))
+
+    def test_article_contents(self):
+        # Get article saved
+        article_saved = Article.objects.get(title=self.article.title)
+        self.assertEqual(article_saved.body, self.article.body)
+        self.assertEqual(article_saved.image_credit, self.article.image_credit)
+        self.assertTrue(article_saved.tags)
+        self.assertEqual(article_saved.status, self.article.status)
+        self.assertEqual(f'{article_saved.read_time}', f'{self.article.read_time}')
+        self.assertTrue(os.path.exists(f'{settings.MEDIA_ROOT}'+
+                                       f'{self.article.image}'))
+
+    @classmethod
+    def tearDownClass(cls):
+        media_root = settings.MEDIA_ROOT
+        # Remove test media root
+        shutil.rmtree(media_root)
