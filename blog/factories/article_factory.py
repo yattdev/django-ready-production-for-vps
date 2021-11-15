902037a52732bc5763f80706bdb99ef9c12203d0
yattdev <yattdeveloper@gmail.com>
2021-11-15 19:37:33 +0100

    Added factories for blog article models

---
+++ blog/factories/article_factory.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import factory
+import random
+from faker import Faker
+from blog.models.article_models import Article
+from .category_factory import CategoryFactory
+from .author_factory import AuthorFactory
+from django.core.files.base import ContentFile
+
+# Creat object faker
+fake = Faker()
+
+
+class ArticleFactory(factory.django.DjangoModelFactory):
+    """Factory models for blog:article models """
+    class Meta:
+        model = Article
+        # Solution for unique contraint field
+        django_get_or_create('author',)
+
+    category = factory.SubFactory(CategoryFactory)
+    title = fake.name()
+    author = factory.SubFactory(AuthorFactory)
+    image = factory.LazyAttribute(
+            lambda _: ContentFile(
+                factory.django.ImageField()._make_data(
+                    {'width': 1024, 'height': 768}
+                ), 'example.jpg'
+            )
+        )
+    image_credit = fake.name()
+    body = fake.paragraph(nb_sentences=100)
+    tags = fake.name()
+    status = 'Publish'
+    read_time = random.randrange(6)
+
+
