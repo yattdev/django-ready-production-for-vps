2512b0e8f9fdd2dfb4028460011d889b4099b5d0
yattdev <yattdeveloper@gmail.com>
2021-11-16 15:51:36 +0100

    Update comment test with Faker and factory_boy

--- blog/tests/models/test_comment_models.py
+++ blog/tests/models/test_comment_models.py
@@ -1,27 +1,44 @@
+import os
+import shutil
+
 # Core Django imports.
-from django.test import TestCase
+from django.conf import settings
+from django.test import TestCase, override_settings
 
-# Third-party Django app imports.
-from model_mommy import mommy
-
+from blog.factories.comment_factory import CommentFactory
 # Blog application imports.
 from blog.models import Article
 from blog.models.comment_models import Comment
 
 
+@override_settings(MEDIA_ROOT=os.path.join(settings.BASE_DIR,
+                                           'media_dir_for_test/'))
 class CommentTestCase(TestCase):
     """
       Class to test the Blog model.
     """
-
-    def setUp(self):
+    @classmethod
+    def setUpTestData(cls):
         """
           Set up all the tests using model_mommy.
         """
-        self.article = mommy.make(_model=Article, body="Test")
-        self.comment = mommy.make(Comment, article=self.article)
+        cls.comment = CommentFactory()
 
     def test_if_comment_returns_the_right_user(self):
         self.assertEqual(
             self.comment.__str__(),
             f"Comment by {self.comment.name} on {self.comment.article}")
+
+    def test_comment_content(self):
+        comment_saved = Comment.objects.get(email=self.comment.email)
+        self.assertEqual(f'{comment_saved.comment}', f'{self.comment.comment}')
+        self.assertEqual(f'{comment_saved.article.title}',
+                         f'{self.comment.article.title}')
+        self.assertTrue(comment_saved.approved)
+
+    @classmethod
+    def tearDownClass(cls):
+        # Delete test media directory
+
+        if os.path.exists(settings.MEDIA_ROOT):
+            shutil.rmtree(settings.MEDIA_ROOT)
