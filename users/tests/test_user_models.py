057b2faed45cd49512f8aae88030c2b15a6e0089
yattdev <yattdeveloper@gmail.com>
2021-11-18 22:03:57 +0100

    From users:test:UserAccountTestCase change methode test_user_contents to small piece au test_cases

--- users/tests/test_user_models.py
+++ users/tests/test_user_models.py
@@ -1,5 +1,3 @@
-import os
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
@@ -11,27 +9,55 @@ User = get_user_model()
 
 class UserAccountTestCase(TestCase):
     """ TestCase for UserAccount models"""
-    def setUp(self):
-        self.user = UserFactory()
-        self.admin = AdminFactory()
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory()
+        cls.admin = AdminFactory()
+
+        # --- Get instance saved from db
+        cls.user_saved = User.objects.get(id=cls.user.id)
+        cls.superuser_saved = User.objects.get(id=cls.admin.id)
 
     def test_if_users_created(self):
         self.assertEqual(User.objects.count(), 2)
 
-    def test_user_content(self):
-        user = User.objects.get(id=self.user.id)
+    # --------------- User TestCase ---------------
+    def test_user_if_first_name_saved(self):
+        self.assertEqual(f'{self.user_saved.first_name}',
+                         f'{self.user.first_name}')
 
-        self.assertEqual(f'{user.first_name}', f'{self.user.first_name}')
-        self.assertEqual(f'{user.last_name}', f'{self.user.last_name}')
-        self.assertEqual(f'{user.password}', f'{self.user.password}')
-        self.assertEqual(f'{user.username}', f'{self.user.username}')
-        self.assertEqual(f'{user.email}', f'{self.user.email}')
+    def test_user_if_last_name_saved(self):
+        self.assertEqual(f'{self.user_saved.last_name}',
+                         f'{self.user.last_name}')
 
-    def test_superuser_content(self):
-        superuser = User.objects.get(id=self.admin.id)
+    def test_user_if_password_saved(self):
+        self.assertEqual(f'{self.user_saved.password}',
+                         f'{self.user.password}')
 
-        self.assertEqual(f'{superuser.first_name}', f'{self.admin.first_name}')
-        self.assertEqual(f'{superuser.last_name}', f'{self.admin.last_name}')
-        self.assertEqual(f'{superuser.password}', f'{self.admin.password}')
-        self.assertEqual(f'{superuser.username}', f'{self.admin.username}')
-        self.assertEqual(f'{superuser.email}', f'{self.admin.email}')
+    def test_user_if_username_saved(self):
+        self.assertEqual(f'{self.user_saved.username}',
+                         f'{self.user.username}')
+
+    def test_user_if_email_saved(self):
+        self.assertEqual(f'{self.user_saved.email}', f'{self.user.email}')
+
+    # --------------- Superuser TestCase ---------------
+    def test_if_superuser_first_name_saved(self):
+        self.assertEqual(f'{self.superuser_saved.first_name}',
+                         f'{self.admin.first_name}')
+
+    def test_if_superuser_last_name_saved(self):
+        self.assertEqual(f'{self.superuser_saved.last_name}',
+                         f'{self.admin.last_name}')
+
+    def test_if_superuser_password_name_saved(self):
+        self.assertEqual(f'{self.superuser_saved.password}',
+                         f'{self.admin.password}')
+
+    def test_if_superuser_username_saved(self):
+        self.assertEqual(f'{self.superuser_saved.username}',
+                         f'{self.admin.username}')
+
+    def test_if_superuser_email_saved(self):
+        self.assertEqual(f'{self.superuser_saved.email}',
+                         f'{self.admin.email}')
