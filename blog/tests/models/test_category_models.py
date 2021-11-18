e27a297744f383cbf931bab75ba16b68f2543a14
yattdev <yattdeveloper@gmail.com>
2021-11-18 21:58:21 +0100

    From blog:test:models:CategoryTestCase change methode test_profile_contents to small piece au test_cases

--- blog/tests/models/test_category_models.py
+++ blog/tests/models/test_category_models.py
@@ -24,31 +24,40 @@ class CategoryTestCase(TestCase):
         """
         cls.category = CategoryFactory()
 
+        # ----- Get saved Category
+        cls.cat_saved = Category.objects.get(name=cls.category.name)
+
+    def test_if_category_image_exist(self):
+        self.assertTrue(
+            os.path.exists(f'{settings.MEDIA_ROOT}' +
+                           f'{self.cat_saved.image}'))
+
+    def test_if_category_name_not_empty(self):
+        self.assertFalse(self.cat_saved.name == '')
+
     def test_if_category_returns_the_right_human_readable_representation(self):
-        self.assertEqual(self.category.__str__(), self.category.name)
+        self.assertEqual(self.cat_saved.__str__(), self.cat_saved.name)
 
     def test_if_category_returns_the_right_slug(self):
-        self.assertEqual(self.category.slug, slugify(self.category.name))
+        self.assertEqual(self.cat_saved.slug, slugify(self.cat_saved.name))
 
     def test_category_get_absolute_url(self):
         self.assertEquals(
-            self.category.get_absolute_url(),
+            self.cat_saved.get_absolute_url(),
             reverse('blog:category_articles',
-                    kwargs={'slug': self.category.slug}))
+                    kwargs={'slug': self.cat_saved.slug}))
 
-    def test_category_contents(self):
-        # Get category saved
-        cat_saved = Category.objects.get(name=self.category.name)
-        self.assertFalse(cat_saved.approved)
+    def test_if_category_not_approuved(self):
+        self.assertFalse(self.cat_saved.approved)
+
+    def test_if_category_image_tag_exist(self):
         self.assertTrue(
-            os.path.exists(f'{settings.MEDIA_ROOT}' + f'{cat_saved.image}'))
-        self.assertTrue(
-            f'{cat_saved.image_tag()}',
-            f'<img src="/media/{cat_saved.image}" width="150" height="80"/>')
+            f'{self.cat_saved.image_tag()}',
+            f'<img src="/media/{self.cat_saved.image}" \
+            width="150" height="80"/>')
 
     @classmethod
     def tearDownClass(cls):
         # Delete test media directory
-
         if os.path.exists(settings.MEDIA_ROOT):
             shutil.rmtree(settings.MEDIA_ROOT)
