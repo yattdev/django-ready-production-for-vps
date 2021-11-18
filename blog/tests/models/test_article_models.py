59a474a74416d546c1db89857d60cd6b9391f83d
yattdev <yattdeveloper@gmail.com>
2021-11-18 21:51:18 +0100

    From blog:test:models:ArticleTestCase change methode test_article_contents to small piece au test_cases

--- blog/tests/models/test_article_models.py
+++ blog/tests/models/test_article_models.py
@@ -25,6 +25,9 @@ class ArticleTestCase(TestCase):
         """
         cls.article = ArticleFactory()
 
+        # Get article saved
+        cls.article_saved = Article.objects.get(title=cls.article.title)
+
     def test_if_article_returns_the_right_human_readable_representation(self):
         self.assertEqual(self.article.__str__(), self.article.title)
 
@@ -40,21 +43,29 @@ class ArticleTestCase(TestCase):
                         'slug': self.article.slug
                     }))
 
-    def test_article_contents(self):
-        # Get article saved
-        article_saved = Article.objects.get(title=self.article.title)
-        self.assertEqual(article_saved.body, self.article.body)
-        self.assertEqual(article_saved.image_credit, self.article.image_credit)
-        self.assertTrue(article_saved.tags)
-        self.assertEqual(article_saved.status, self.article.status)
-        self.assertEqual(f'{article_saved.read_time}',
+    def test_if_article_body_correctly_saved(self):
+        self.assertEqual(self.article_saved.body, self.article.body)
+
+    def test_if_article_image_credit_correctly_saved(self):
+        self.assertEqual(self.article_saved.image_credit,
+                         self.article.image_credit)
+
+    def test_if_article_tags_correctly_saved(self):
+        self.assertTrue(self.article_saved.tags)
+
+    def test_if_article_status_correctly_saved(self):
+        self.assertEqual(self.article_saved.status, self.article.status)
+
+    def test_if_article_read_time_correctly_saved(self):
+        self.assertEqual(f'{self.article_saved.read_time}',
                          f'{self.article.read_time}')
+
+    def test_if_article_image_correctly_saved(self):
         self.assertTrue(
             os.path.exists(f'{settings.MEDIA_ROOT}' + f'{self.article.image}'))
 
     @classmethod
     def tearDownClass(cls):
         # Delete test media directory
-
         if os.path.exists(settings.MEDIA_ROOT):
             shutil.rmtree(settings.MEDIA_ROOT)
