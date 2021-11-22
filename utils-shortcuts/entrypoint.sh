a32ce44b682057f2fe0e81403af85924bd7a6e86
yattdev <yattdeveloper@gmail.com>
2021-11-22 13:04:09 +0100

    Update utils-shortcuts/entrypoint to run test in each build and run

--- utils-shortcuts/entrypoint.sh
+++ utils-shortcuts/entrypoint.sh
@@ -5,13 +5,17 @@ then
     echo "Waiting for postgres..."
 
     while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+        echo "Waiting for postgres..."
       sleep 0.1
     done
 
     echo "PostgreSQL started"
 fi
 
-python manage.py flush --no-input
+# python manage.py flush --no-input
 python manage.py migrate
+
+# run test
+python manage.py test
 
 exec "$@"
