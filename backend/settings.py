00ed4bb18891f519cf99d850ef5a8fcc54268f58
yattdev <yattdeveloper@gmail.com>
2021-11-27 22:41:08 +0100

    Added Celery stuff configutation to settings

--- backend/settings.py
+++ backend/settings.py
@@ -332,3 +332,12 @@ if os.environ.get('ENV') == 'PRODUCTION':
     EMAIL_USE_TLS = True
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# Celery stuff configutation
+#  BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = "redis://redis:6379"
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
