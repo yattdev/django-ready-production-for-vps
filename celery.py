b4123e9b8e41e88e283726a1631b0693b2415c8a
yattdev <yattdeveloper@gmail.com>
2021-11-27 22:41:53 +0100

    Update celery.py to add lambda to autodiscover_tasks arg

--- celery.py
+++ celery.py
@@ -3,9 +3,10 @@
 
 import os
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 app = Celery('backend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
