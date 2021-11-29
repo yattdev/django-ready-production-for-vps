cd7506328d120d2b81889fce47ab723e78da48c8
yattdev <yattdeveloper@gmail.com>
2021-11-29 10:27:46 +0100

    Deleted root/__init__.py, that was a mistake i did, when adding celery
    - Added it's content to root/backend/__init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from .celery import app as celery_app
-
-__all__ = ['celery_app']
---
+++ backend/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from backend.celery import app as celery_app
+
+__all__ = ['celery_app']
