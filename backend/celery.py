613d0b587691045408e5d7e19bd0a4fad53736a6
yattdev <yattdeveloper@gmail.com>
2021-11-29 10:29:47 +0100

    Moved root/celery.py to root/backend/celery.py

--- backend/celery.py
+++ backend/celery.py
@@ -3,10 +3,14 @@
 
 import os
 from celery import Celery
-from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 app = Celery('backend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
