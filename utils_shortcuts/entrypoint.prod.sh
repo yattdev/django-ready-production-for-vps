8458358790014ad12b3997fe6a68fdd7fce17db6
yattdev <yattdeveloper@gmail.com>
2021-12-13 17:05:21 +0100

    Update entrypoint.prod.sh
    - Added script that run juste on startup, the mentioned script is django
    migrate, test, and collectstatic

--- utils_shortcuts/entrypoint.prod.sh
+++ utils_shortcuts/entrypoint.prod.sh
@@ -7,14 +7,23 @@ then
     while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
     done
-
     echo "PostgreSQL started"
+
 fi
 
-# Update database with migrate
-python manage.py migrate --noinput --traceback
+# This condition if juste to run following on container startup
+if ! $INITIALIZE
+then
+    # Update database with migrate
+    python manage.py migrate --noinput --traceback
 
-# Run test with test
-# python manage.py test --noinput --traceback
+    # Run test with test
+    python manage.py test --noinput --traceback
+
+    # Collectstatic files with collectstatic command
+    python manage.py collectstatic --noinput --traceback
+
+    export INITIALIZE="DONE"
+fi
 
 exec "$@"
