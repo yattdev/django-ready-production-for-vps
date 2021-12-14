1d268c1c3e5c1248d059a9ba6e8ab34002eb8bdc
yattdev <yattdeveloper@gmail.com>
2021-12-14 10:15:04 +0100

    Update entrypoint.prod.sh to add sleep between instructions

--- utils_shortcuts/entrypoint.prod.sh
+++ utils_shortcuts/entrypoint.prod.sh
@@ -1,28 +1,37 @@
 #!/bin/sh
 
+# This variable is to define first startup
+CONTAINER_ALREADY_STARTED=".CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
+
+    if ! nc -z $SQL_HOST $SQL_PORT; then
+        echo $CONTAINER_ALREADY_STARTED
+        rm -f .CONTAINER_ALREADY_STARTED_PLACEHOLDER
+    fi
+    sleep 1
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
     done
 
     echo "PostgreSQL started"
+    sleep 1
 fi
 
 # Following statements will be run just once time, that's container startup
-CONTAINER_ALREADY_STARTED=".CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     echo "-- First container startup --"
     # Update database with migrate
-    python manage.py migrate --noinput --traceback
+    python manage.py migrate --noinput --traceback && sleep 1
 
     # Run test with test
-    python manage.py test --noinput --traceback
+    python manage.py test --noinput --traceback && sleep 1
 
     # Run collectstatic with collectstatic
-    python manage.py collectstatic --noinput --traceback
+    python manage.py collectstatic --noinput --traceback && sleep 1
 
     # --------
     touch $CONTAINER_ALREADY_STARTED
