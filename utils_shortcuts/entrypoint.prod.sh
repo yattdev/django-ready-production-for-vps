921d66c920dac95d34f5b2601bb26f884aa3e512
yattdev <yattdeveloper@gmail.com>
2021-12-14 09:37:09 +0100

    Added a statements to entrypoint.prod.sh
    - To run django command only once time on container startup,

--- utils_shortcuts/entrypoint.prod.sh
+++ utils_shortcuts/entrypoint.prod.sh
@@ -7,14 +7,27 @@ then
     while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
     done
-    echo "PostgreSQL started"
 
+    echo "PostgreSQL started"
 fi
 
-# Update database with migrate
-python manage.py migrate --noinput --traceback
+# Following statements will be run just once time, that's container startup
+CONTAINER_ALREADY_STARTED=".CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    echo "-- First container startup --"
+    # Update database with migrate
+    python manage.py migrate --noinput --traceback
 
-# Collectstatic files with collectstatic command
-python manage.py collectstatic --noinput --traceback
+    # Run test with test
+    python manage.py test --noinput --traceback
+
+    # Run collectstatic with collectstatic
+    python manage.py collectstatic --noinput --traceback
+
+    # --------
+    touch $CONTAINER_ALREADY_STARTED
+else
+    echo "-- Not first container startup --"
+fi
 
 exec "$@"
