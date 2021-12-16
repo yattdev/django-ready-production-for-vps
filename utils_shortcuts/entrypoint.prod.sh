37071b459dff6c78de5c17812ba6bc2892e44b8d
yattdev <yattdeveloper@gmail.com>
2021-12-16 10:17:30 +0100

    Update utils_shortcuts/prepareVenv_and_up_docker and entrypoint.prod.sh
    - utils_shortcuts/prepareVenv_and_up_docker
        To delete web services scripts
    - entrypoint.prod.sh
        To added migrate, test and collectstatic scripts
        - This work now, and failed before because of change celery service dockerfile
        inside of docker-compose.override for testing

--- utils_shortcuts/entrypoint.prod.sh
+++ utils_shortcuts/entrypoint.prod.sh
@@ -11,24 +11,22 @@ then
         echo "Delete file:" $CONTAINER_ALREADY_STARTED
         rm -f .CONTAINER_ALREADY_STARTED_PLACEHOLDER
     fi
-    sleep 1
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
     done
 
     echo "PostgreSQL started"
-    sleep 1
 fi
 
 # Following statements will be run just once time, that's container startup
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     echo "-- First container startup --"
     # Update database with migrate
-    # python manage.py migrate --noinput --traceback && sleep 1
+    python manage.py migrate --noinput --traceback && sleep 1
 
     # Run test with test
-    # python manage.py test --noinput --traceback && sleep 1
+    python manage.py test --noinput --traceback && sleep 1
 
     # Run collectstatic with collectstatic
     python manage.py collectstatic --noinput --traceback && sleep 1
